47ce786e2865b34af420e4d9a587497e337ff058
svenugoban <asvenugoban19962015@gmail.com>
2024-07-29 23:50:40 +0530

    Create a service to store and retrieve jokes.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # submit-jokes-microservice
+
+# Install Nest.js CLI globally
+npm install -g @nestjs/cli
+
+# Create a new Nest.js project
+nest new project-name
+
+# Move into the project directory
+cd project-name
+
+# Install dependencies
+npm install
+
+# Run the development server
+npm run start
--- submit-jokes-microservice/src/app.service.ts
+++ submit-jokes-microservice/src/app.service.ts
@@ -1,8 +1,16 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private jokes = [];
+
+  submitJoke(jokeDto: { joke: string; type: string }): string {
+    this.jokes.push(jokeDto);
+    return 'Joke submitted successfully';
+  }
+
+  getJokes(): any[] {
+    return this.jokes;
   }
 }
