0c28f044286314e8b968414f7ce54f1f328cbf41
svenugoban <asvenugoban19962015@gmail.com>
2024-07-29 23:50:15 +0530

    Create a controller to handle submissions.

--- submit-jokes-microservice/src/app.controller.ts
+++ submit-jokes-microservice/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('submit')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('joke')
+  submitJoke(@Body() jokeDto: { joke: string, type: string }): string {
+    return this.appService.submitJoke(jokeDto);
   }
 }
